32c03f85d53c3c072bd937b89494f70a7d0cec7c
Jay Patel <jaypatel122700@gmail.com>
2021-06-03 15:43:37 -0400

    made some optimizations

--- sieve.py
+++ sieve.py
@@ -4,22 +4,47 @@ import math
 import numpy as np
 import time
 
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
+
 def list_primes(int : max) -> np.array:
-	primes = np.array(range(2,max+1)) # this is intially the list of primes
+	primes = np.arange(2,max+1) # this is intially the list of primes
 	num = 2 # start with 2
 	# only need to cross out multiples for nums less than sqrt(max)
 	while num <= math.ceil(np.sqrt(max)): 
 		multiple = num*2 # the first multiple in question
 		while (multiple <= max): # until the end of the list
-			ind = np.searchsorted(primes, multiple) 
-			# binary search to find where multiple would be if it was in the list
-			# now, if you have found the multiple then remove it
-			if ind < primes.size and primes[ind] == multiple:
-				primes = np.delete(primes, ind)  
+			ind = np.where(primes == multiple) # see if you find the multiple
+			if ind[0].size != 0: # now, if you have found the multiple then remove it
+				primes[ind[0]] = 0
 			multiple += num
-		ind = np.searchsorted(primes, num) # find the next present number in the list 
-		num = primes[ind+1] if ind <= primes.size else max
-	return primes
+		ind = np.array([])
+		while num <= max and ind.size == 0:
+			num += 1
+			ind = np.where(primes == num)[0]
+		printProgressBar(num, math.ceil(np.sqrt(max)))
+	return primes[np.nonzero(primes)]
 
 def save(int : max):
 	f = open('primes_up_to_' + str(max) + '.txt', 'w')
@@ -28,7 +53,7 @@ def save(int : max):
 		f.write(str(prime) + '\n')
 
 if __name__ == '__main__':
-	max = int(input())
+	max = int(input('Up to what number? '))
 	initial = time.perf_counter()
 	save(max)
 	final = time.perf_counter()
